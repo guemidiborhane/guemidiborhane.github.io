683b806411b73d1469b61836741a5e972d3072a5
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2020-03-23 00:16:05 +0100

    The same thing we do every night, Pinky - try to take over the world!

---
+++ data.js
@@ -0,0 +1,35 @@
+const main = {
+  name: 'Borhaneddine GUEMIDI',
+  mail: 'guemidiborhane@gmail.com',
+  img: 'https://www.gravatar.com/avatar/1aeb75e3e1bd8cb0e2c93eff4c38e8ca/?s=512&d=mp&r=g',
+  role: ['Full Stack Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://profile.codersrank.io/user/guemidiborhane'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/guemidiborhane'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/guemidiborhane/'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/guemidiborhane'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'guemidiborhane@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: []
+};
